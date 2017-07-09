da5f96540a8dc1a0b39db1e8a2078cda7f729222
Bill Swearingen <hevnsnt@gmail.com>
2017-07-09 14:10:54 -0500

    Adding files

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# SecKCESP8266
-SecKC Challenge
+# ESP8266 Mobile Rick Roll Captive Portal
+### Live Chat
+[![Gitter](https://badges.gitter.im/idolpx/mobile-rr.svg)](https://gitter.im/idolpx/mobile-rr?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+
+The purpose of this project was to help me get familiar with programming for the ESP8266 and have a bit of fun.
+
+[![ESP8266 Mobile-RR](https://i.ytimg.com/vi/ZpcRZoXQAzM/hqdefault.jpg)](https://www.youtube.com/watch?v=ZpcRZoXQAzM)
+
+* User connects to the device broadcasting an SSID of "FREE Highspeed WiFi" (this is configurable of course)
+* The captive portal helper on their phone/table/computer kicks in and presents them with a page showing "Terms of Service" and a button labeled "I Accept"
+* When they click the button a full screen animated GIF of Rick Astley dancing appears and an audio clip of "Never Gonna Give You Up" starts playing on loop along with a message in the bottom right letting them know they got rock rolled (Incase they don't understand what's going on)
+
+On bootup, the buzzer plays a little bit of "Never Gonna Give You Up".
+To access the console, connect to the ESP8266 Access Point and browse to "http://10.10.10.1/console".
+From here you can monitor all connections and see when someone gets Rick Roll'd by the device.
+The console also allows you to enter commands to get more info about the status of the device, change the SSID, get the Rick Roll count and even beep the buzzer.
+
+The "debug" setting is on by default and shows you all DNS and HTTP requests made to the device. It's kind of cool to see all the sites that the apps on your phone are trying to access. Check out the screenshots of the console for a glimpse of what it looks like. Some apps are very noisy. You can toggle the "debug" off to not see that stuff.
+
+## Parts List
+* ESP8266 Module with 4MB of flash  (I used the WeMos D1 Mini) (http://www.aliexpress.com/snapshot/7833150367.html?orderId=76398745536320)
+* Piezo buzzer (http://www.aliexpress.com/snapshot/7762649061.html?orderId=75910217556320)
+* USB battery pack with micro-usb cable to power everything
+
+## Wiring
+Connect the '+' lead of the piezo to GPIO 4 (D2 on WeMos D1 Mini) and '-' lead to Ground.
+I chose GPIO 4 because I installed the long leads with the headers on the WeMos D1 Mini. The spacing from ground was perfect to just plug the buzzer in direct between G & D2.
+
+## Build Firmware
+I use PlatformIO to build this.  http://platformio.org/
+
+* Install PlatformIO and let it update itself on first startup
+* Once updated and restarted, clone the "mobile-rr" project to a folder and open the project in PlatformIO
+* Next build the firmware by clicking the checkmark icon on the toolbar
+
+![Build Firmware](https://s20.postimg.org/e9mna84pp/build_firmware.png)
+
+## Upload Firmware and SPIFFS data
+After your firmware build is successful you can upload it by clicking the arrow under the checkmark in the PlatformIO toolbar.
+
+![Upload Firmware](https://s20.postimg.org/ue4gppiot/upload_firmware.png)
+
+You can add/edit the files in the "www" folder to your liking. (Files in the "www" folder will be cloned and gzipped to the "data" folder when building.) Then follow the instructions below to build and upload the SPIFFS file system image to your ESP8266.
+
+![Upload SPIFFS 1](https://s20.postimg.org/p1ymo5v4t/build_spiffs.png)
+
+![Upload SPIFFS 2](https://s20.postimg.org/vrw3l0hy5/image.png)
+
+**Note: Anytime you make changes to the firmware or the data you can rebuild and upload either without the need to install the other again. They reside in different areas of the flash memory.**
+
+## References
+I learned everything I needed to create this from these projects
+
+- PlatformIO - http://platformio.org/
+-              http://docs.platformio.org/en/latest/platforms/espressif.html#uploading-files-to-file-system-spiffs
+- ESP8266 Captive Portal - https://www.hackster.io/rayburne/esp8266-captive-portal-5798ff
+- ESP-RADIO - https://github.com/Edzelf/Esp-radio
+- ESPAsyncWebServer - https://github.com/me-no-dev/ESPAsyncWebServer
+- SOFTAP - Get List of Connected Clients - http://www.esp8266.com/viewtopic.php?f=32&t=5669
+- How to print free memory - http://forum.wemos.cc/topic/175/how-to-print-free-memory
+- WebSocketToSerial - https://github.com/hallard/WebSocketToSerial.git
+- JQueryTerminal - https://github.com/jcubic/jquery.terminal
+- OTA Update - http://www.penninkhof.com/2015/12/1610-over-the-air-esp8266-programming-using-platformio/
+- Piezo Beep - http://framistats.com/2015/06/07/esp8266-arduino-smtp-server-part-2/
+- Simple Audio Board - http://bitcows.com/?p=19
+- Tone Doesn't Work - https://www.reddit.com/r/esp8266/comments/46kw38/esp8266_calling_tone_function_does_not_work/
+- ArduinoJson - https://github.com/bblanchon/ArduinoJson
+- EEPROM - https://gist.github.com/dogrocker/f998dde4dbac923c47c1
+- Exception Causes - https://github.com/esp8266/Arduino/blob/master/doc/exception_causes.md
+- WiFi Scan- https://www.linuxpinguin.de/project/wifiscanner/
+- SPIFFS - https://github.com/esp8266/Arduino/blob/master/doc/filesystem.md
+-          http://blog.squix.org/2015/08/esp8266arduino-playing-around-with.html
+- WiFiManager - https://github.com/tzapu/WiFiManager
+- ESP-GDBStub - https://github.com/esp8266/Arduino/tree/master/libraries/GDBStub
---
+++ www_proc.py
@@ -0,0 +1,210 @@
+#
+# Clone files from "www" to "data" folder
+# Then compress and embed resources
+#
+from SCons.Script import DefaultEnvironment
+from distutils import dir_util
+from shutil import copyfileobj, move, rmtree
+import base64, gzip, os, sys, re
+
+def gzFile(file):
+    with open(file, 'rb') as f_in, gzip.open(file + '.gz', 'wb') as f_out:
+        copyfileobj(f_in, f_out)
+    os.remove(file)
+
+def read_file(file):
+    with open(file, 'r') as myfile:
+        content = myfile.read()
+    return content.strip()
+
+def read_media(file):
+    with open(file, 'rb') as myfile:
+        content = base64.b64encode(myfile.read())
+    return content
+
+def get_mimetype(file):
+    filename, file_extension = os.path.splitext(file)
+    return {
+        '.gif': 'image/gif',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.svg': 'image/svg+xml',
+        '.mp3': 'audio/mp3'
+    }[file_extension]
+
+def minify_css(css):
+    # remove comments - this will break a lot of hacks :-P
+    css = re.sub( r'\s*/\*\s*\*/', "$$HACK1$$", css ) # preserve IE<6 comment hack
+    css = re.sub( r'/\*[\s\S]*?\*/', "", css )
+    css = css.replace( "$$HACK1$$", '/**/' ) # preserve IE<6 comment hack
+
+    # url() doesn't need quotes
+    css = re.sub( r'url\((["\'])([^)]*)\1\)', r'url(\2)', css )
+
+    # spaces may be safely collapsed as generated content will collapse them anyway
+    css = re.sub( r'\s+', ' ', css )
+
+    # shorten collapsable colors: #aabbcc to #abc
+    css = re.sub( r'#([0-9a-f])\1([0-9a-f])\2([0-9a-f])\3(\s|;)', r'#\1\2\3\4', css )
+
+    # fragment values can loose zeros
+    css = re.sub( r':\s*0(\.\d+([cm]m|e[mx]|in|p[ctx]))\s*;', r':\1;', css )
+
+    for rule in re.findall( r'([^{]+){([^}]*)}', css ):
+
+        # we don't need spaces around operators
+        selectors = [re.sub( r'(?<=[\[\(>+=])\s+|\s+(?=[=~^$*|>+\]\)])', r'', selector.strip() ) for selector in rule[0].split( ',' )]
+
+        # order is important, but we still want to discard repetitions
+        properties = {}
+        porder = []
+        for prop in re.findall( '(.*?):(.*?)(;|$)', rule[1] ):
+            key = prop[0].strip().lower()
+            if key not in porder: porder.append( key )
+            properties[ key ] = prop[1].strip()
+
+        # output rule if it contains any declarations
+        if properties:
+            print "%s{%s}" % ( ','.join( selectors ), ''.join(['%s:%s;' % (key, properties[key]) for key in porder])[:-1] )
+
+    return css
+
+def embed_css(content):
+    pattern = re.compile(ur'<link.*href=[\'"](.*?)[\'"].*/>')
+
+    # Combine CSS
+    matches = re.findall(pattern, content)
+    css_content = ''
+    for match in matches:
+        css_content = css_content + '<style>\n' + read_file(data + match) + '\n</style>\n'
+        os.remove(data + match)
+        print match
+
+    if len(css_content.strip()):
+        # Remove CSS includes from Content
+        content = pattern.sub('', content)
+
+        css_content = minify_css(css_content)
+        pattern = re.compile(ur'</head>')
+        content = pattern.sub(css_content + '\n</head>', content)
+
+    return content
+
+
+def combine_js(content):
+    # Combine Javascript
+    pattern = re.compile(ur'<script.*src=[\'"](.*?)[\'"].*?</script>')
+    matches = re.findall(pattern, content)
+    js_content = ''
+    for match in matches:
+        js_content = js_content + read_file(data + match) + '\n'
+        os.remove(data + match)
+        print match
+
+    # Save combined file
+    if len(js_content.strip()):
+        with open(data + "script.js", 'w') as new_file:
+            # Write out new line
+            new_file.write( js_content )
+        new_file.close()
+        if re.search(r'gz', options):
+            gzFile( data + "script.js" )
+
+        # Remove Javascript includes from Content
+        content = pattern.sub('', content)
+
+    # Update HTML Content
+    if len(js_content.strip()):
+        pattern = re.compile(ur'</head>')
+        content = pattern.sub(ur'<script src="script.js"></script>\n</head>', content)
+
+    return content
+
+def embed_media(content):
+    pattern = re.compile(ur'<(?:img|audio).*src=[\'"](.*?)[\'"].*>')
+
+    # Combine CSS
+    matches = re.findall(pattern, content)
+    media_content = ''
+    for match in matches:
+        media_content = read_media(data + match)
+        media_content = "data:" + get_mimetype(match) + ";base64," + media_content
+        os.remove(data + match)
+        print match
+        content = re.sub(match, media_content, content)
+
+    return content
+
+# Build httpdocs for web server
+def before_buildfs(source, target, env):
+    print "before_buildfs"
+
+# SPIFFS Stats With Different Combinations of Processing
+# Updated: 12.28.2016
+# No Processing
+#   20 Files, 1.46 MB of 2.81 MB Used
+# custom_option = "gz"
+#   19 Files, 898.84 KB of 2.81 MB Used
+# custom_option =   "gz|css"
+#   17 Files, 896.88 KB of 2.81 MB Used
+# custom_option = "gz|css|js"
+#   13 Files, 893.94 KB of 2.81 MB Used
+# custom_option = "gz|css|js|media"
+#   8 Files, 898.60 KB of 2.81 MB Used
+
+    # clone 'www' folder to 'data' folder
+    files = dir_util.copy_tree(www, data, )
+
+    # embed Javascript, CSS & media into html files
+    if re.search(r'css|js|media', options):
+        for file in files:
+            if re.search(r'\.htm', file):
+                print file
+                content = read_file(file)
+                if re.search(r'css', options):
+                    content = embed_css( content )
+                if re.search(r'js', options):
+                    content = combine_js( content )
+                if re.search(r'media', options):
+                    content = embed_media( content )
+
+                # Save New HTML File
+                with open(file, 'w') as new_file:
+                    new_file.write( content )
+                new_file.close()
+
+    # gzip appropriate files
+    if re.search(r'gz', options):
+        pattern = re.compile(ur'\.htm|\.css|\.js|\.map|\.svg|\.ico')
+        for file in files:
+            if re.search(pattern, file):
+                if os.path.exists(file):
+                    print file
+                    gzFile( file )
+
+
+# remove 'data' folder after upload
+def after_uploadfs(source, target, env):
+    print "after_uploadfs"
+    rmtree(data)
+
+
+env = DefaultEnvironment()
+options = base64.b64decode(ARGUMENTS.get("CUSTOM_OPTION"))
+
+# Set parameters from environment variables
+data = env['PROJECT_DIR'] + "/data/"
+www = env['PROJECT_DIR'] + "/www/"
+
+# Show parameters
+print data
+print www
+print options
+
+# Only run when building & uploading SPIFFS image
+#env.AddPreAction("buildfs", before_buildfs)
+env.AddPreAction("$BUILD_DIR/spiffs.bin", before_buildfs)
+env.AddPostAction("uploadfs", after_uploadfs)
+
+#if 'SPIFFS_START' in env:
+#   before_buildfs("","buildfs",env)
